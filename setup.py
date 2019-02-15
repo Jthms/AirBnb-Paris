3ee946e792c466bb234a762c7c89b96474996bcd
Jthms <thomasjulia44@ymail.com>
2019-02-15 13:31:14 +0100

    Premier commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Probability of resiliation of a home-insurance',
+    author='Mickaël JUILLET, Ilze SULTE, Julia THOMAS',
+    license='MIT',
+)
